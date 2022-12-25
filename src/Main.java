6c8a37d94ad0ca042087e2047a26dbe55d7177a6
a123 <shchevelev.roman@yandex.ru>
2022-12-25 11:22:12 +0300

    5 коммит

--- src/Main.java
+++ src/Main.java
@@ -5,32 +5,45 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         Employee[] employees = new Employee[10];
-        employees[0] = new Employee("Иван Иванов Иванович", 10_000, 1);
-        employees[1] = new Employee("Петр Петров Петрович", 10_000, 2);
-        employees[2] = new Employee("Сидр Сидоров Сидорович", 10_000, 3);
-        employees[3] = new Employee("Дмитрий Дмитриев Дмитриевич", 10_000, 4);
-        employees[4] = new Employee("Василий Василенко Васильевич", 10_000, 5);
-        employees[5] = new Employee("Роман Романов Романович", 10_000, 5);
-        employees[6] = new Employee("Леонид Даниленко Леонидович", 10_000, 4);
-        employees[7] = new Employee("Иван Грудов Иванович", 10_000, 3);
-        employees[8] = new Employee("Виктор Орлов Викторович", 10_000, 2);
-        employees[9] = new Employee("Вячеслав Рудаков Вячеславович", 10_000, 1);
-    }
-    public static int amountSalaryOfAllEmployee(Employee[] employees){
-        int sum = 0;
-        for (Employee employee: employees){
-            if (employee != null){
-                sum += employee.getSalary();
+        {
+            employees[0] = new Employee("Иван Иванов Иванович", 10_000, 1);
+            employees[1] = new Employee("Петр Петров Петрович", 10_000, 2);
+            employees[2] = new Employee("Сидр Сидоров Сидорович", 10_000, 3);
+            employees[3] = new Employee("Дмитрий Дмитриев Дмитриевич", 10_000, 4);
+            employees[4] = new Employee("Василий Василенко Васильевич", 10_000, 5);
+            employees[5] = new Employee("Роман Романов Романович", 10_000, 5);
+            employees[6] = new Employee("Леонид Даниленко Леонидович", 10_000, 4);
+            employees[7] = new Employee("Иван Грудов Иванович", 10_000, 3);
+            employees[8] = new Employee("Виктор Орлов Викторович", 10_000, 2);
+            employees[9] = new Employee("Вячеслав Рудаков Вячеславович", 10_000, 1);
+            // Вывод на печать всех сотрудников
+            for (int i = 0; i < employees.length; i++) {
+                System.out.println(employees[i]);
             }
+            //Вывод на печать всех сотрудников
+            double sum = 0;
+            for (int i = 0; i < employees.length; i++) {
+                sum += employees[i].getSalary();
+            }
+            System.out.println(sum);
         }
-        return sum;
-    }
-    public void printSumSalary(){
-        System.out.println(amountSalaryOfAllEmployee());
-    }
-    public Employee printAllUsers(Employee[] employees) {
+        // Вывод на печать сотрудника с минимальной заработной платой
+        double min = -1;
         for (int i = 0; i < employees.length; i++) {
-            System.out.println(employees[i]);
+            if (employees[i].getSalary() < min) {
+                min = employees[i].getSalary();
+                {
+                    System.out.println(min);
+                }
+            }
+            // Вывод на печать сотрудника с максимальной заработной платой
+            double max = min;
+            if (employees[i].getSalary() > max) {
+                max = employees[i].getSalary();
+                {
+                    System.out.println(max);
+                }
+            }
         }
     }
 }
